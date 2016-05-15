8306a3499c13cee7ae7b2851051a66d27f48d998
Chris Perez <chris.k.perez@gmail.com>
2016-05-14 18:49:10 -0700

    most of app complete. getting weird error in write function

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const read = require('./lib/read');
+const create_new = require('./lib/create_new');
+
+read('./img/palette-bitmap.bmp');
--- transformer.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const fs = require('fs');
-const image = fs.readFile(__dirname + '/img/non-palette-bitmap.bmp');
-const headers = {};
